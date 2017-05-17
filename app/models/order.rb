a085363229a81451a8a2ef07ecdb5f2b16ed7501
dbshooy <dbshooy@gmail.com>
2017-05-17 09:20:50 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  before_create :generate_token
 
   belongs_to :user
 
@@ -7,4 +8,8 @@ class Order < ApplicationRecord
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
   has_many :product_lists
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
